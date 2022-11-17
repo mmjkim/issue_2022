d1d554e506a1af7faab6d8a17699ba7b1cd56dbf
mjkim <mjkim@limesolution.co.kr>
2022-11-17 15:50:07 +0900

    테이블 셀 높이 수정, 정렬 추가

--- view/frm_anal_s.py
+++ view/frm_anal_s.py
@@ -146,6 +146,9 @@ class Ui_Anal_Dialog(object):
         QtCore.QMetaObject.connectSlotsByName(Anal_Dialog)
 
         self.tbl_naver.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.tbl_naver.verticalHeader().setDefaultSectionSize(35)
+        self.tbl_word.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.tbl_word.verticalHeader().setDefaultSectionSize(35)
 
         self.btn_coll.clicked.connect(self.get_data)
         self.btn_anal.clicked.connect(self.get_anal)
@@ -274,6 +277,7 @@ class Ui_Anal_Dialog(object):
         self.btn_coll_c.setText(_translate("Anal_Dialog", "데이터 수집"))
         self.label_3.setText(_translate("Anal_Dialog", "뉴스 크롤링 데이터"))
         self.tbl_naver.setSortingEnabled(True)
+        self.tbl_word.setSortingEnabled(True)
 
 
 if __name__ == "__main__":
